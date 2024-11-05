5b93471423fca01c8b2c504bfe0770b6b133e34a
Fajar Sidik <46022001+Sidigcoy@users.noreply.github.com>
2024-11-06 01:06:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Dear Debbi",
+    introTitle: 'Untuk Debby Marisca kesayanganku',
+    introDesc: `Aku mau kasih tau kamu sesuatu, kamu penasaran ga? kalo engga close aja yah`,
+    btnIntro: 'MAU!',
+    title: ' 🥰',
+    desc: 'Kamu pantas mendapatkan yang terbaik, seseorang yang akan mendukungmu selalu, membiarkanmu tumbuh tanpa batas, dan mencintaimu tanpa akhir. Apakah kamu akan ijinin aku jadi orang itu?',
+    btnYes: 'Iyaaa dongs! <3',
+    btnNo: 'Najis hih!',
+    question: 'kamu mau jadi pacarku? kalo boleh tau alesannya apa tuh? wkwk',
+    btnReply: 'Submit',
+    reply: 'Karena kamu tuh ganteng banget',
+    mess: 'iya doong WKWWKKW, noted!',
+    messDesc: 'Terima kasih banyak yaahhh 😘',
+    btnAccept: 'okok <3',
+    messLink: 'https://api.whatsapp.com/send?phone=6282299082415&text=Ya%20aku%20mau%20terima%20kak%20fajar%20yg%20ganteng' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
